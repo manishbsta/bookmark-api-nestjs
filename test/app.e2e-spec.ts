bee51178e1f7dc168c8da66a7c414bcc36fe3596
Manish Bista <bistamanish681@gmail.com>
2023-03-09 16:53:36 +0545

    test cases for auth

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,13 @@
-import { INestApplication } from '@nestjs/common';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
+import * as pactum from 'pactum';
+import { AuthDto } from '../src/auth/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -16,11 +19,108 @@ describe('App e2e', () => {
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
     await app.init();
+    await app.listen(3333);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl('http://localhost:3333');
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('should pass');
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'mb@gmail.com',
+      password: '123456',
+    };
+
+    describe('Signup', () => {
+      it('should throw 400 if email is not provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({ password: dto.password })
+          .expectStatus(400);
+      });
+
+      it('should throw 400 if password is not provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({ email: dto.email })
+          .expectStatus(400);
+      });
+
+      it('should throw 400 if body is not provided', () => {
+        return pactum.spec().post('/auth/signup').expectStatus(400);
+      });
+
+      it('should sign up', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Login', () => {
+      it('should throw 400 if email is not provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/login')
+          .withBody({ password: dto.password })
+          .expectStatus(400);
+      });
+
+      it('should throw 400 if password is not provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/login')
+          .withBody({ email: dto.email })
+          .expectStatus(400);
+      });
+
+      it('should throw 400 if body not provided', () => {
+        return pactum.spec().post('/auth/login').expectStatus(400);
+      });
+
+      it('should log in', () => {
+        return pactum
+          .spec()
+          .post('/auth/login')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('user_access_token', 'access_token');
+      });
+    });
+
+    describe('Get Profile', () => {
+      it('should get the user details', () => {
+        return pactum
+          .spec()
+          .get('/auth/profile')
+          .withHeaders({
+            Authorization: 'Bearer $S{user_access_token}',
+          })
+          .expectStatus(200);
+      });
+    });
+  });
+
+  // describe('Bookmarks', () => {
+  //   describe('Create Bookmark', () => {
+  //     it.todo('');
+  //   });
+
+  //   describe('Get Bookmark', () => {});
+
+  //   describe('Get Bookmark by Id', () => {});
+
+  //   describe('Edit Bookmark', () => {});
+
+  //   describe('Delete Bookmark', () => {});
+  // });
 });
